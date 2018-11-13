ebf149168636527697479edf5b5e879c55261a3a
Airton Ponce <airtonponce@outlook.com>
2018-11-13 17:22:21 -0200

    implemented omniauth gem & database is working

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,13 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def instagram
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
-    # if @user.persisted?
     if @user.new_record?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "instagram") if is_navigational_format?
-    else
-      session["devise.instagram_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-
+      @user.save
+      session[:user_return_to] = root_path
     end
+    sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+    set_flash_message(:notice, :success, kind: "instagram") if is_navigational_format?
   end
 
   def failure
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :omniauthable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :omniauth_providers => [:instagram]
+         :recoverable, :rememberable, :validatable, :omniauth_providers => [:instagram]
 
 def self.from_omniauth(auth)
-  # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
-
-    user.email = auth.info.email
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.nickname + "@mapa.com"
     user.password = Devise.friendly_token[0, 20]
 
     # user.name = auth.info.name   # assuming the user model has a name
