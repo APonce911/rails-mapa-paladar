b99834e391eb39180a878d2a931995d605a7feb9
gabriel schiavo <gabriel@lewagon.org>
2018-11-13 14:44:01 -0200

    Branch to work wuith front

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'privacypolicy', to: "pages#privacypolicy"
   devise_for :users, :path => 'users'
-
+  get 'components', to: "pages#components"
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
