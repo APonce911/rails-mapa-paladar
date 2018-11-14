f2c3ea1d89e6eac8d1700c7032cbaf8ad2bc6d9b
Airton Ponce <airtonponce@outlook.com>
2018-11-14 20:56:25 -0200

    added loop - parsing only when has geocode

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,33 +6,40 @@ before_action :parse_posts
 
   def create
     create_posts
-    create_images
   end
 
 private
 
   def create_posts
-    @post = Post.create(user_id: current_user.id)
-    unix_time = @posts["data"][0]["created_time"].to_i
-    @post[:date] = Time.at(unix_time).to_datetime
-    @post[:ig_id] = @posts["data"][0]["id"]
-    @post[:text] = @posts["data"][0]["caption"]["text"]
-    @post[:lat] = @posts["data"][0]["location"]["latitude"]
-    @post[:lng] = @posts["data"][0]["location"]["longitude"]
-    @post[:restaurant_name] = @posts["data"][0]["location"]["name"]
-    @post.save
+    @posts["data"].each_with_index do |element, index|
+
+      if element["location"] != nil
+        @post = Post.create(user_id: current_user.id)
+        @post[:ig_id] = element["id"]
+        unix_time = element["created_time"].to_i
+        @post[:date] = Time.at(unix_time).to_datetime
+        @post[:text] = element["caption"]["text"]
+        @post[:lat] = element["location"]["latitude"]
+        @post[:lng] = element["location"]["longitude"]
+        @post[:restaurant_name] = element["location"]["name"]
+        @post.save
+        create_images(index)
+      end
+    end
+
   end
 
-  def create_images
-    if @posts["data"][0]["images"].class == Array
-      @posts["data"][0]["images"].each do |element|
+  def create_images(index)
+
+    if @posts["data"][index]["images"].class == Array
+      @posts["data"][index]["images"].each do |element|
         image = Image.create(post_id: @post.id)
         image["url"] = element["standard_resolution"]["url"]
         image.save
         end
     else
       image = Image.create(post_id: @post.id)
-      image[:url] = @posts["data"][0]["images"]["standard_resolution"]["url"]
+      image[:url] = @posts["data"][index]["images"]["standard_resolution"]["url"]
       image.save
     end
   end
