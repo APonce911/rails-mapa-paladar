e7be794406994f0be2e35e5d838422aaf1c63a1e
gabriel schiavo <gabriel@lewagon.org>
2018-11-16 12:38:57 -0200

    Finished custom marker icon

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -4,16 +4,28 @@ import GMaps from 'gmaps/gmaps.js';
 let map = document.getElementById('map')
 if (map) {
 
-    // Coordinates to center the map
+  // Coordinates to center the map
   var myLatlng = new google.maps.LatLng(41.8874314503,12.4886930452);
 
-    // Other options for the map, pretty much selfexplanatory
+  // Other options for the map, pretty much selfexplanatory
   var mapOptions = {
         zoom: 16,
         center: myLatlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
+  // CUSTOM MARKER
+  var icon = {
+    path: "M0,2 L0,51.3964714 C-3.8565838e-15,51.9487562 0.44771525,52.3964714 1,52.3964714 L2.0863,52.3964714 C2.36766,52.3964714 2.59574,52.6237893 2.59574,52.9041748 L2.59574,62 L18.5881,52.6744493 C18.9003,52.4923743 19.2555,52.3964714 19.6172,52.3964714 L52,52.3964714 C53.1045695,52.3964714 54,51.5010409 54,50.3964714 L54,5 C54,2.23857625 51.7614237,4.39800865e-15 49,0 L2,0 C0.8954305,2.02906125e-16 -1.3527075e-16,0.8954305 0,2 Z",
+        fillColor: '#DD5A58',
+        fillOpacity: 0.9,
+        anchor: new google.maps.Point(0,10),
+        strokeWeight: 0,
+        // scale: iconSize
+        borderStyle: 'solid' 
+  }
+  
+  // STYLE
   const style = {
       styles: [
       {
@@ -133,16 +145,7 @@ if (map) {
   
   // var teste  <%= @teste %>;
   
-
-  // CUSTOM MARKER
-  var icon = {
-    path: "M0 0V52.231H2.0863C2.36766 52.231 2.59574 52.4576 2.59574 52.7371V61.8042L18.5881 52.5081C18.9003 52.3266 19.2555 52.231 19.6172 52.231H54V0H0Z",
-        fillColor: '#DD5A58',
-        fillOpacity: .6,
-        anchor: new google.maps.Point(0,0),
-        // strokeWeight: 0,
-        // scale: iconSize
-  }
+ 
   // var marker = new google.maps.Marker({position: myLatlng, map: map, animation: google.maps.Animation.BOUNCE});
   // NEW MARKER FUNCTION
   var marker = new google.maps.Marker({position: myLatlng,
