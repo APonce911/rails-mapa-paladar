b5f7215927b8c2710e9f9c026b5710cc72400c8e
Airton Ponce <airtonponce@outlook.com>
2018-11-19 14:54:42 -0200

    personalized token depending on user.email

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :privacypolicy]
 
   def home
+
     @posts = Post.all.to_json.html_safe
     # como a tabela de images esta na mesma ordem dos posts, e nenhum post tem duas imagens podemos entregar o Image.all sem ter que fazer um query
     @images = Image.all.to_json.html_safe
