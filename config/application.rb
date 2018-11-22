08ab1053fd3e417ac4d987a4f013849715430e43
Airton Ponce <airtonponce@outlook.com>
2018-11-22 18:43:39 -0200

    config rack ssl enforcer

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RailsMapaPaladar
   class Application < Rails::Application
+    config.middleware.insert_before  ActionDispatch::Cookies, Rack::SslEnforcer
     # config.action_view.embed_authenticity_token_in_remote_forms = true
     config.generators do |generate|
           generate.assets false
